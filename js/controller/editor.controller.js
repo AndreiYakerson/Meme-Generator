8a5ee6628e5a9075e621944466c01676d55e4087
AndreiYakerson <tubikwheels@gmail.com>
2025-04-26 21:29:41 +0300

    Move the canvas render into the render editor func

--- js/controller/editor.controller.js
+++ js/controller/editor.controller.js
@@ -5,12 +5,8 @@
 
 
 function onInit(ev) {
-
-    // renderEditorSection()
-
-    gElCanvas = document.querySelector('.canvas')
-    gCtx = gElCanvas.getContext('2d')
-    resizeCanvas()
+    // renderGallerySection()
+    renderEditorSection(gSelectedImgSrc)
 
 }
 
