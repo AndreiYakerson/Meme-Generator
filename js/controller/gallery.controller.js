2dcfda010183c88c0115161719d71bf85162351f
AndreiYakerson <tubikwheels@gmail.com>
2025-04-26 21:31:21 +0300

    Add gallery render func

---
+++ js/controller/gallery.controller.js
@@ -0,0 +1,23 @@
+
+
+
+
+function onGalleryClick() {
+    renderGallerySection()
+}
+
+function renderGallerySection() {
+    
+    let elEditorSection = document.querySelector('.editor-section')
+    let elGallerySection = document.querySelector('.gallery-section')
+
+    elEditorSection.innerHTML = ''
+
+    
+    gImgs.forEach((img,idx) => {
+        console.log(img.src);
+        
+        elGallerySection.innerHTML += `<img id="${idx+1}" onclick="onGalleryImgClick(this)" class="img-gallery" src="${img.src}">`
+    })
+
+}
