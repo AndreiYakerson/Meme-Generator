850ec51798b69d5965400199e98de7769ceb3f10
AndreiYakerson <tubikwheels@gmail.com>
2025-05-09 23:33:45 +0300

    Add random id funcs

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1,1 +1,22 @@
 'use strict'
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); 
+}
+
+function getRandomId() {
+    const chars = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    let id = ''
+
+    for (let i = 0; i < 7; i++) {
+        const curChar = chars.charAt([getRandomInt(0,chars.length - 1)]);
+        id += curChar
+    }
+
+    return id
+}
+
+
